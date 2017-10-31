a113ba8642b1d4e598f797b8c4ed7bef0fea059f
Onat Bas <git@onat.me>
2017-10-31 14:39:31 +0300

    (delete) deleted historyitem package
           Reasons
    =====================
    1. It's duplicate. I don't know why it got merged in the first place.

--- backend/eureka/historyitems/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HistoryitemsConfig(AppConfig):
-    name = 'historyitems'
--- backend/eureka/historyitems/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from historyitems import views
-
-urlpatterns = [
-    url(r'^historyitem', views.historyitems, name='historyitems'),
-]
